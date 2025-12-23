2b2ccefd46dfe22632d031a1df83b2e7b4963218
Allah Rakha <128797491+allahrakha-7@users.noreply.github.com>
2025-12-24 00:32:41 +0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (50.69%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        //base case
+        
         if (n == 1) return true;
 
-        // check if n less than or equal to 0 and modulus of n is not equal to 0
-        if (n <= 0 || n % 2 != 0) return false;
+        if (n <= 0) return false;
 
-        // recursive call
+        if (n % 2 != 0) return false;
+
         return isPowerOfTwo(n / 2);
     }
 };
