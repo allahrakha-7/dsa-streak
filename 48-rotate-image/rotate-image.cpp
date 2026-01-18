5550ecfbb1aa30b6c27f3209ed89eb305eba3859
Allah Rakha <128797491+allahrakha-7@users.noreply.github.com>
2026-01-19 00:28:32 +0500

    Time: 0 ms (100.00%) | Memory: 10.3 MB (9.20%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -4,11 +4,17 @@ public:
         
         for (int i = 0; i < matrix.size(); i++) {
 
-            for (int j = i + 1; j < matrix.size(); j++) {
+            for (int j = i; j < matrix.size(); j++) {
 
                 swap(matrix[i][j], matrix[j][i]);
+
             }
+        }
+        
+        for (int i = 0; i < matrix.size(); i++) {
+            
             reverse(matrix[i].begin(), matrix[i].end());
+
         }
     }
 };
