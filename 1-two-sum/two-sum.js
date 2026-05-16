dd6bf91977565d4475e6a08a0c9e2249f06e914c
Allah Rakha <128797491+allahrakha-7@users.noreply.github.com>
2026-05-16 13:24:46 +0500

    Time: 2 ms (65.91%) | Memory: 54.5 MB (71.20%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,19 +4,36 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
-    let indices = [];
+
+    let indicesMap = new Map();
 
     for (let i = 0; i < nums.length; i++) {
 
-        for (let j = i + 1; j < nums.length; j++) {
+        let difference = target - nums[i];
 
-            if (nums[i] + nums[j] === target) {
+        if (indicesMap.has(difference)) {
 
-                indices.push(i, j)
-
-            }
+            return [indicesMap.get(difference), i];
+        
         }
+        indicesMap.set(nums[i], i);
     }
-    return indices;
+    return [];
+
+    // *** Brute Force ***
+    
+    // let indices = [];
+
+    // for (let i = 0; i < nums.length; i++) {
+
+    //     for (let j = i + 1; j < nums.length; j++) {
+
+    //         if (nums[i] + nums[j] === target) {
+
+    //             indices.push(i, j)
+
+    //         }
+    //     }
+    // }
+    // return indices;
 };
