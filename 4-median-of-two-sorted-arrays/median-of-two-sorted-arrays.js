b49ce7c48afe6d5094023f5356cde0ea21cdb6cc
Allah Rakha <128797491+allahrakha-7@users.noreply.github.com>
2026-07-12 10:05:53 +0500

    Time: 2 ms (87.92%) | Memory: 57.7 MB (86.27%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -4,40 +4,27 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    
+
+    let merged = [];
     let i = 0;
     let j = 0;
 
-    let mergedArray = [];
-
     while (i < nums1.length && j < nums2.length) {
-
         if (nums1[i] < nums2[j]) {
-
-            mergedArray.push(nums1[i++]); 
-        
+            merged.push(nums1[i++]);
         } else {
-
-            mergedArray.push(nums2[j++]);
-
+            merged.push(nums2[j++]);
         }
     }
 
-    while (i < nums1.length) mergedArray.push(nums1[i++]);
-    
-    while (j < nums2.length) mergedArray.push(nums2[j++]);
+    while (i < nums1.length) merged.push(nums1[i++]);
+    while (j < nums2.length) merged.push(nums2[j++]);
 
-    
-    let n = mergedArray.length;
+    let n = merged.length;
 
     if (n % 2 === 1) {
-
-        return mergedArray[parseInt(n / 2)];
-
+        return merged[Math.floor(n / 2)];
     } else {
-
-        return (mergedArray[n / 2 - 1] + mergedArray[n / 2]) / 2.0;
-
+        return (merged[ Math.floor(n / 2 - 1)] + merged[Math.floor(n / 2)]) / 2.0;
     }
-
 };
