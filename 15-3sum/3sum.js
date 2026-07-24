19e0a98568bbe9977b4a64a75ede068d91019e33
Allah Rakha <128797491+allahrakha-7@users.noreply.github.com>
2026-07-24 17:20:41 +0500

    Time: 29 ms (93.44%) | Memory: 72.2 MB (62.90%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -13,40 +13,55 @@ var threeSum = function(nums) {
     // loop through the array
     for (let i = 0; i < nums.length - 2; i++) {
 
+        // check if the current number is greater than 0 then break 
         if (nums[i] > 0) break;
 
+        // check if the i greater than 0 && the current num is equal to the prev num then continue
         if (i > 0 && nums[i] === nums[i - 1]) continue;
 
+        // initialize two pointers 
         let left = i + 1;
         let right = nums.length - 1;
 
+        // loop through the pointers
         while (left < right) {
             
+            // calculate sum of current number and left and right pointers
             const sum = nums[i] + nums[left] + nums[right];
 
+            // check if sum is equal to 0
             if (sum === 0) {
 
+                // push the current number and left and right pointers in the result array
                 result.push([nums[i], nums[left], nums[right]]);
 
+                // increment the left pointer
                 left++;
+                // decrement the right pointer
                 right--;
 
+                // while loop through the pointers && check for left pointer num is equal to previous left pointer num
                 while (left < right && nums[left] === nums[left - 1]) {
 
+                    // increment left pointer
                     left++;
 
                 }
 
+            // else if sum less than 0
             } else if (sum < 0) {
 
+                // increment left pointer
                 left++;
 
             } else {
 
+                // else decrement the right pointer
                 right--;
 
             }
         }
     }
+    // return result array at last
     return result;
 };
