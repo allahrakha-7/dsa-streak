0a2ae83a05504eb34cc502bf3d046334858357ab
Allah Rakha <128797491+allahrakha-7@users.noreply.github.com>
2026-07-24 17:11:14 +0500

    Time: 19 ms (99.98%) | Memory: 69.9 MB (98.49%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    
+    // sort the array
+    nums.sort((a, b) => a - b);
+
+    // create an array result
+    const result = [];
+
+    // loop through the array
+    for (let i = 0; i < nums.length - 2; i++) {
+
+        if (nums[i] > 0) break;
+
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+
+                result.push([nums[i], nums[left], nums[right]]);
+
+                left++;
+                right--;
+
+                while (left < right && nums[left] === nums[left - 1]) {
+
+                    left++;
+
+                }
+
+            } else if (sum < 0) {
+
+                left++;
+
+            } else {
+
+                right--;
+
+            }
+        }
+    }
+    return result;
+};
