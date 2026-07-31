8e009e041dde6bc2bc1a9809c4608e90bff03ef7
Allah Rakha <128797491+allahrakha-7@users.noreply.github.com>
2026-07-31 16:47:19 +0500

    Time: 95 ms (14.53%) | Memory: 63.4 MB (5.05%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -4,19 +4,22 @@
  */
 var lengthOfLongestSubstring = function(s) {
 
-    let charSet = new Set();
+    // edge case
+    if (s.length === 0) return 0;
+
+    const charSet = new Set();
 
     let left = 0;
     let maxLength = 0;
 
     for (let right = 0; right < s.length; right++) {
-
+        
         while (charSet.has(s[right])) {
-
+            
             charSet.delete(s[left]);
-
+            
             left++;
-
+            
         }
         charSet.add(s[right]);
 
